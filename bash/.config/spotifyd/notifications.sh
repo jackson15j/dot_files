7e781145e1cc41885a034b08247e4b492ce777a1
Craig Astill <craig.astill@gmail.com>
2020-12-03 15:14:35 +0000

    Add `spotifyd` track length to notifications.
    Using `printf` float, so getting: `3.24` instead of `3:24`.

--- bash/.config/spotifyd/notifications.sh
+++ bash/.config/spotifyd/notifications.sh
@@ -10,11 +10,12 @@ then
   track_number=$(playerctl metadata | grep trackNumber |awk '{print $3}')
   url=$(playerctl metadata | grep url |awk '{print $3}')
   art_url=$(playerctl metadata | grep artUrl |awk '{print $3}')
+  track_length=$(printf %.2f $(echo "$(playerctl metadata | grep length |awk '{print $3}')/60000000"| bc -l))
   wget -O /tmp/art $art_url
 
   # https://askubuntu.com/questions/189231/where-are-the-stock-icon-names-defined-for-the-unity-panel-service-indicators-an
   notify-send -a spotifyd -i /tmp/art -c applications-multimedia -u low \
               "Track: $track_number $trackName" \
-              "Artist: $artist\nAlbum: $album\nUrl: $url"
-  echo "$trackName - $artistAndAlbumName" > /tmp/i3/status/spotifyd
+              "Artist: $artist\nAlbum: $album\nLength: $track_length\nUrl: $url"
+  echo "$track_number. $trackName - $artistAndAlbumName ($track_length)" > /tmp/i3/status/spotifyd
 fi
