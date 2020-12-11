5583f9f343d22dc57442f0847a6a548ad36e513f
Craig Astill <craig.astill@gmail.com>
2020-12-11 13:58:03 +0000

    Explicit about using spotifyd for `playerctl` based notifications.
    Had updated Microsoft Teams, which is now seen by playerctl as a chromium
    instance. Since `playerctl` seems to pick players alphabetically, this broke my
    notifications.

--- bash/.config/spotifyd/notifications.sh
+++ bash/.config/spotifyd/notifications.sh
@@ -3,14 +3,14 @@
 mkdir -p /tmp/i3/status
 if [ "$PLAYER_EVENT" = "start" ] || [ "$PLAYER_EVENT" = "change" ];
 then
-  trackName=$(playerctl metadata title)
-  artist=$(playerctl metadata artist)
-  album=$(playerctl metadata album)
-  artistAndAlbumName=$(playerctl metadata --format "{{ artist }} ({{ album }})")
-  track_number=$(playerctl metadata | grep trackNumber |awk '{print $3}')
-  url=$(playerctl metadata | grep url |awk '{print $3}')
-  art_url=$(playerctl metadata | grep artUrl |awk '{print $3}')
-  track_length=$(printf %.2f $(echo "$(playerctl metadata | grep length |awk '{print $3}')/60000000"| bc -l))
+  trackName=$(playerctl -p spotifyd  metadata title)
+  artist=$(playerctl -p spotifyd  metadata artist)
+  album=$(playerctl -p spotifyd  metadata album)
+  artistAndAlbumName=$(playerctl -p spotifyd  metadata --format "{{ artist }} ({{ album }})")
+  track_number=$(playerctl -p spotifyd  metadata | grep trackNumber |awk '{print $3}')
+  url=$(playerctl -p spotifyd  metadata | grep url |awk '{print $3}')
+  art_url=$(playerctl -p spotifyd  metadata | grep artUrl |awk '{print $3}')
+  track_length=$(printf %.2f $(echo "$(playerctl -p spotifyd  metadata | grep length |awk '{print $3}')/60000000"| bc -l))
   wget -O /tmp/art $art_url
 
   # https://askubuntu.com/questions/189231/where-are-the-stock-icon-names-defined-for-the-unity-panel-service-indicators-an
